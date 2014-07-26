02562b381ae7d80e745d52132ff010ff6aa96423
jef26 <jef265@gmail.com>
2014-07-26 09:58:41 -0400

    Programming Assignment 2: Lexical Scoping
    cache matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## creates a special matrix object that caches its inverse
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix(), tol = sqrt(.Machine$double.eps)) {
+        dnx <- dimnames(x)
+        if(is.null(dnx)) dnx <- vector("list", 2)
+        s <- svd(x)
+        nz <- s$d > tol * s$d[1]
+        structure(
+                if(any(nz)) s$v[, nz] %*% (t(s$u[, nz])/s$d[nz]) else x,
+                dimnames = dnx[2:1])
+        }
+get <- function() x
+setinv <- function(nz) m <<- nz
+getinv <- function() m
+list(set = set, get = get,
+     setinv = setinv,
+     getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## computes the inverse of the special matrix.  
+## If inverse has already been calculated matrix is not changed. 
 
 cacheSolve <- function(x, ...) {
+        m <- x$getinv()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- inv(data, ...)
+        x$setinv(m)
+        m
         ## Return a matrix that is the inverse of 'x'
+        solve(x)
 }
